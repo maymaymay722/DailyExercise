bcf20a82567572142562b9916cc56b2a26d86bd7
maymaymay722 <meiyingying0722@163.com>
2020-04-19 17:50:38 +0800

    selenium的API

--- 20200414/src/__init__.py
+++ 20200414/src/__init__.py
@@ -1,17 +1,9 @@
+import unittest
 from selenium import webdriver
 import time,os
 
-
-driver=webdriver.Chrome()
-file_path="file:///"+os.path.abspath("checkbox.html")
-driver.get(file_path)
-driver.implicitly_wait(10)
-
-inputs=driver.find_elements_by_tag_name("input")
-for input in inputs:
-    if input.get_attribute('type')=="checkbox":
-        input.click()
-time.sleep(6)
-driver.quit()
-
-
+class chandao(unittest.TestCase):
+    def setUp(self):
+        self.driver=webdriver.Firefox()
+        self.driver.implicitly_wait(10)
+        self.base_url="http://127.0.0.1/"
--- 20200414/src/test_send.py
+++ 20200414/src/test_send.py
@@ -5,9 +5,10 @@ driver=webdriver.Firefox()
 file_path='file:///'+os.path.abspath("send.html")
 driver.get(file_path)
 
-driver.find_element_by_xpath("html/body/input").click()
+driver.find_element_by_xpath("/html/body/input").click()
 time.sleep(5)
-driver.switch_to.alert.send_keys('webdriver')
+alert=driver.switch_to.alert
+alert.send_keys("虞书欣")
 time.sleep(5)
 driver.switch_to.alert.accept()
 time.sleep(5)
